2a21e0eb74a9ce7118f1da38608d85e6153736ce
Sebastian Kürten <sebastian@topobyte.de>
2016-09-06 16:09:12 +0200

    Added the CoordinateGeometryTransformer

---
+++ src/main/java/de/topobyte/jts/utils/transform/CoordinateGeometryTransformer.java
@@ -0,0 +1,58 @@
+// Copyright 2016 Sebastian Kuerten
+//
+// This file is part of jts-utils.
+//
+// jts-utils is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jts-utils is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with jts-utils. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.jts.utils.transform;
+
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.CoordinateSequenceFactory;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.util.GeometryTransformer;
+
+import de.topobyte.jgs.transform.CoordinateTransformer;
+
+/**
+ * @author Sebastian Kuerten (sebastian@topobyte.de)
+ */
+public class CoordinateGeometryTransformer extends GeometryTransformer
+{
+
+	private CoordinateTransformer ct;
+
+	/**
+	 * @param ct
+	 *            the coordinate transformation to use.
+	 */
+	public CoordinateGeometryTransformer(CoordinateTransformer ct)
+	{
+		this.ct = ct;
+	}
+
+	@Override
+	protected CoordinateSequence transformCoordinates(
+			CoordinateSequence coords, Geometry parent)
+	{
+		CoordinateSequenceFactory csf = factory.getCoordinateSequenceFactory();
+		CoordinateSequence cs = csf
+				.create(coords.size(), coords.getDimension());
+		for (int i = 0; i < coords.size(); i++) {
+			cs.setOrdinate(i, 0, ct.getX(coords.getX(i)));
+			cs.setOrdinate(i, 1, ct.getY(coords.getY(i)));
+		}
+		return cs;
+	}
+
+}
